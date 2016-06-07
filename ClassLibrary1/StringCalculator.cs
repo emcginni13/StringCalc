601aca404ffef6875ea8a2869d0cb652cada8049
emcginni13 <emcginnis@unionhomemortgage.com>
2016-06-07 10:00:13 -0400

    First commit

---
+++ ClassLibrary1/StringCalculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class StringCalculator
+    {
+        public static int Calculate(string numbersToAdd)
+        {
+            string negatives=null;
+            string[] delimArray = new string[4];
+            int total = 0;
+            string delimitor = ",";
+            if (numbersToAdd.Contains("\n"))
+            {
+               delimitor = "\n";
+            }
+            if (numbersToAdd == "") { return 0; }
+            if (numbersToAdd.StartsWith("//"))
+            {
+                delimitor = GetCustomDelim(numbersToAdd);
+                if (delimitor.Contains("["))
+                {
+                    int i = 0;
+                    var multipleDelimsStrings = delimitor.Split('[');
+                    foreach (var s in multipleDelimsStrings)
+                    {
+                        if (s.Contains("]")) { delimArray[i] = s.Substring(0, s.IndexOf(']')); }
+                        i++;
+                    }
+                    for (var index = 1; index < delimArray.Length; index++)
+                    {
+                        numbersToAdd = numbersToAdd.Replace(delimArray[index], ",");
+                    }
+                }
+                numbersToAdd = numbersToAdd.Substring(numbersToAdd.IndexOf('\n')+1);             
+            }
+            
+            foreach (var num in GetNumbers(numbersToAdd,delimitor))
+            {
+                if (int.Parse(num) > 1000) { total = total - int.Parse(num); }
+                if (int.Parse(num) < 0) { negatives +=',' + num + " "; } 
+                total = total + int.Parse(num);
+            }
+            if (negatives != null) { throw new ArgumentException("Your calculation included the invalid negative number(s) " + negatives ); }
+            return  total;
+        }
+        public static string[] GetNumbers(string numbersBefore, string delimitor)
+        {
+            numbersBefore = numbersBefore.Replace(delimitor, ",");
+            var numbers = numbersBefore.Split(',');
+            return numbers;
+        }
+        public static string GetCustomDelim(string numbersToAdd)
+        {
+            var index = numbersToAdd.IndexOf('\n');
+            var delimitor = numbersToAdd.Substring(2, index - 2);
+            return delimitor;
+        }
+    }
+}
---
+++ StringCalculatorTest/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace StringCalculatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        public void TwoNumberString()
+        {
+            Assert.AreEqual(7, StringCalculator.Calculate("3 , 4"));
+        }
+        [TestMethod]
+        public void TwoNumberString2()
+        {
+            Assert.AreEqual(9, StringCalculator.Calculate("5 , 4"));
+        }
+        [TestMethod]
+        public void BlankString()
+        {
+            Assert.AreEqual(0, StringCalculator.Calculate(""));
+        }
+        [TestMethod]
+        public void OneNumberString()
+        {
+            Assert.AreEqual(5, StringCalculator.Calculate("5"));
+        }
+        [TestMethod]
+        public void NewLineDelimitor()
+        {
+            Assert.AreEqual(9, StringCalculator.Calculate("5 \n 4"));
+        }
+        [TestMethod]
+        public void CustomDelimitor()
+        {
+            Assert.AreEqual(10, StringCalculator.Calculate("//JimIsAwesome\n5JimIsAwesome5"));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NegativeInNumbersToAdd()
+        {
+            StringCalculator.Calculate("-2,-3");
+        }
+        [TestMethod]
+        public void IgnoreOverOneThousand()
+        {
+            Assert.AreEqual(5, StringCalculator.Calculate("//JimIsAwesome\n5JimIsAwesome1001"));
+        }
+        [TestMethod]
+        public void MultipleDelimitors()
+        {
+            Assert.AreEqual(34, StringCalculator.Calculate("//[,][$][@]\n10,9$8@7"));
+        }
+    }
+}
